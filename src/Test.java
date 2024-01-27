bf6aafb52c6f497fedcf37c3d68edf5a87c062c2
jitpatelonline24 <146672738+jitpatelonline24@users.noreply.github.com>
2024-01-27 15:14:01 +0000

    commit3

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,7 @@
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("My Name is Jitesh, How are you?");        
+        System.out.println("My Name is Jitesh, How are you?");  
+        System.out.println("I am good, How are you?");  
     }
 }
